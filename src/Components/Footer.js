c834949a9f9d5bf00a1bbdab571a420efd0cc772
Ololawrence <olorunwalaw@gmail.com>
2025-07-07 00:46:43 +0100

    change some import informations

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer footer-center mt-32 mb-10  text-base-content text-lg rounded" >
-            <small className='flex text-md text-[#8892b0]'>Designed & Built by <a className='text-primary' href='https://github.com/Ololawrence' target='_blank' rel='noreferrer'>Olorunwa Lawrence</a> with ❤️</small>
+            <small className='flex text-md text-[#8892b0]'>Designed & Built by <a className='text-primary' href='https://github.com/Ololawrence' target='_blank' rel='noreferrer'>Emmanuel Olorunwa Lawrence</a> with ❤️</small>
         </footer >
     );
 };
--- src/Pages/Home/Education.js
+++ src/Pages/Home/Education.js
@@ -3,38 +3,55 @@ import { BiRightArrow } from 'react-icons/bi';
 
 const Education = () => {
     return (
-        <section className='mb-24' id='education'>
-            <div className='mb-12'>
-                <div className='flex items-center gap-6'>
-                    <h3 className='lg:text-3xl md:text-3xl text-2xl text-accent font-bold'>Education </h3>
-                    <hr className='bg-[#233554] lg:w-80 md:w-64 w-40 h-0.5'></hr>
-                </div>
-                <div className='divide-x bg-[#233554] h-60 w-0.5 ml-[10px] mt-6 fixed left-45'></div>
-            </div>
+      <section className="mb-24" id="education">
+        <div className="mb-12">
+          <div className="flex items-center gap-6">
+            <h3 className="lg:text-3xl md:text-3xl text-2xl text-accent font-bold">
+              Education{" "}
+            </h3>
+            <hr className="bg-[#233554] lg:w-80 md:w-64 w-40 h-0.5"></hr>
+          </div>
+          <div className="divide-x bg-[#233554] h-60 w-0.5 ml-[10px] mt-6 fixed left-45"></div>
+        </div>
 
-            <div>
-                <h3 className='text-accent text-xl font-semibold'>Bachelor of Science, </h3>
-                <h4 className='text-lg my-2'>
-                National Open University</h4>
-                <p className='text-accent'>November 2016 - April 2021</p>
+        <div>
+          <div className="mt-5 grid gap-y-2 ml-3">
+            <h3 className="text-accent text-xl font-semibold">
+              Bachelor of Science,{" "}
+            </h3>
+            <h4 className="text-lg my-2">National Open University</h4>
+            <p className="text-accent">November 2015 - April 2019</p>
+            <p className="text-accent">Guidance and counseling</p>
+          </div>
 
-                <div className='mt-5 grid gap-y-2 ml-3'>
-                    <p className='flex items-center gap-x-3'>
-                        <BiRightArrow className='text-primary text-sm' />
-                        Intensive course with core computer science topics.
-                    </p>
-                    <p className='flex items-center gap-x-3'>
-                        <BiRightArrow className='text-primary text-sm' />
-                       Data Structures, Algorithms, OOP.
-                    </p>
-                  
-                    <p className='flex items-center gap-x-3'>
-                        <BiRightArrow className='text-primary text-sm' />
-                        Different software design patterns.
-                    </p>
-                </div>
-            </div>
-        </section>
+          <div className="mt-5 grid gap-y-2 ml-3">
+            <h3 className="text-accent text-xl font-semibold">Diploma </h3>
+            <h4 className="text-lg my-2">
+              COLLEGE OF AVIATION AND MANAGEMENT STUDIES
+            </h4>
+            <p className="text-accent">2013 - 2015</p>
+            <p className="text-accent">Accounting and finanace</p>
+
+            <h4 className="text-lg my-2">Udemy Advance Javascript</h4>
+          </div>
+
+          <div className="mt-5 grid gap-y-2 ml-3">
+            <p className="flex items-center gap-x-3">
+              <BiRightArrow className="text-primary text-sm" />
+              Intensive course with core computer science topics.
+            </p>
+            <p className="flex items-center gap-x-3">
+              <BiRightArrow className="text-primary text-sm" />
+              Data Structures, Algorithms, OOP.
+            </p>
+
+            <p className="flex items-center gap-x-3">
+              <BiRightArrow className="text-primary text-sm" />
+              Different software design patterns.
+            </p>
+          </div>
+        </div>
+      </section>
     );
 };
 
